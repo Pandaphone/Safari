41eac9a2253930e5408fde05fec912c46fdcb066
Pandaphone <shawngreene075@gmail.com>
2022-08-23 03:34:23 -0400

    Lots of style changes!

---
+++ logic.js
@@ -0,0 +1,4 @@
+let car = document.getElementById("car");
+let enemy1 = document.getElementById("enemy1");
+let enemy2 = document.getElementById("enemy2");
+let enemy3 = document.getElementById("enemy3");
