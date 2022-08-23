d22644d37aa9fb23b40ca99e2ba2cfd7fa0ff31f
Pandaphone <shawngreene075@gmail.com>
2022-08-23 17:27:09 -0400

    Added jumping. Removed images. Most don't work.

--- logic.js
+++ logic.js
@@ -1,4 +1,20 @@
-let car = document.getElementById("car");
+let char = document.getElementById("char");
 let enemy1 = document.getElementById("enemy1");
 let enemy2 = document.getElementById("enemy2");
 let enemy3 = document.getElementById("enemy3");
+
+
+function jumpFunc(){
+    char.classList.add("jumpLogic");
+    setTimeout(function(){
+        char.classList.remove("jumpLogic");
+    }, 3000);
+    
+}
+
+function myFunction() {
+    let popupLogic = document.getElementById("popup");
+    popupLogic.classList.toggle("show");
+  }
+
+  
